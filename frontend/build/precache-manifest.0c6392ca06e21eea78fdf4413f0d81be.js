171101ace90f3e5bb7fc04fe4a0360202ca47467
YONGBAAM <jokester59@naver.com>
2022-03-14 20:54:36 +0900

    prod setting

---
+++ frontend/build/precache-manifest.0c6392ca06e21eea78fdf4413f0d81be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f007ff9d47cfde5a86a9",
+    "url": "/static/js/main.f007ff9d.chunk.js"
+  },
+  {
+    "revision": "55e57a89bb46973763d2",
+    "url": "/static/js/1.55e57a89.chunk.js"
+  },
+  {
+    "revision": "f0d922fac72455845849",
+    "url": "/static/js/2.f0d922fa.chunk.js"
+  },
+  {
+    "revision": "414f310dccc3c02d980a",
+    "url": "/static/js/runtime~main.414f310d.js"
+  },
+  {
+    "revision": "b23ca8fa9aa3deb6d7510dd2cc9fa858",
+    "url": "/index.html"
+  }
+];
